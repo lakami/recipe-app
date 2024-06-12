1ab9f0a72c4eb4082d53df1d96440d76c15d0f94
Kamil Hermann <kamilhermann@op.pl>
2024-06-12 18:14:36 +0200

    secure operation

--- src/main/java/dev/pjatk/recipeapp/usecase/recipe/UpdateRecipeImageUseCase.java
+++ src/main/java/dev/pjatk/recipeapp/usecase/recipe/UpdateRecipeImageUseCase.java
@@ -1,9 +1,11 @@
 package dev.pjatk.recipeapp.usecase.recipe;
 
+import dev.pjatk.recipeapp.exception.ForbiddenModificationException;
 import dev.pjatk.recipeapp.exception.ResourceNotFoundException;
 import dev.pjatk.recipeapp.repository.RecipeRepository;
 import dev.pjatk.recipeapp.service.ImageService;
 import dev.pjatk.recipeapp.util.Loggable;
+import dev.pjatk.recipeapp.util.security.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,8 +17,12 @@ public class UpdateRecipeImageUseCase implements Loggable {
     private final RecipeRepository recipeRepository;
 
     public void execute(Long recipeId, MultipartFile image) {
+
         var recipe = recipeRepository.findById(recipeId)
                 .orElseThrow(() -> new ResourceNotFoundException("Recipe not found"));
+        if (!SecurityUtils.isAdminOrCurrentUser(recipe.getCreatedBy().getEmail())) {
+            throw new ForbiddenModificationException();
+        }
         boolean success;
         var currentImage = recipe.getImageUrl();
         try {
